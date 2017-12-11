a1602f3fb42a92fd0a1130056a34eb61497ee2c8
Ani Gonzales <anisgonzales@gmail.com>
2017-12-11 03:26:32 +0000

    A basic website

---
+++ projects/runtime/js/level01.js
@@ -0,0 +1,50 @@
+(function (window) {
+    window.opspark = window.opspark || {};
+
+    var draw = window.opspark.draw;
+    var createjs = window.createjs;
+
+    window.opspark.runLevelInGame = function(game) {
+        // some useful constants 
+        var groundY = game.groundY;
+
+        // this data will allow us to define all of the
+        // behavior of our game
+        var levelData = {
+            name: "Robot Romp",
+            number: 1, 
+            speed: -3,
+            gameItems: [
+                {type: 'sawblade',x:400,y:groundY},
+                {type: 'sawblade',x:600,y:groundY},
+                {type: 'sawblade',x:900,y:groundY}
+            ]
+        };
+
+        // set this to true or false depending on if you want to see hitzones
+        game.setDebugMode(false);
+
+        // BEGIN EDITING YOUR CODE HERE
+        var createSawBlade = function(x,y) {
+            var hitZoneSize = 25;
+            var damageFromObstacle = 10;
+            var myObstacle = game.createObstacle(hitZoneSize,damageFromObstacle);
+            
+            myObstacle.x = x;
+            myObstacle.y = y;
+            
+            game.addGameItem(myObstacle);
+            
+            var obstacleImage = draw.bitmap('img/sawblade.png');
+            myObstacle.addChild(obstacleImage);
+            
+            obstacleImage.x = -25;
+            obstacleImage.y = -25;
+        }; 
+        
+        
+       levelData.gameItems.forEach(function(gameItem){
+           createSawBlade()
+       });
+    };
+})(window);
